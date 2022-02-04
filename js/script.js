ca537215e3fbd4723b9c02f3daced8ad042d29b9
wudueuk <wudueuk@gmail.com>
2022-02-04 19:30:44 +0200

    add script.js, modified index.html

---
+++ js/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('.modal__checkbox');
+const modalDiscount = document.querySelector('.modal__input_discount');
